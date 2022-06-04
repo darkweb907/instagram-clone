013016e94b7cc9d3c992462d59e8633e8e395b74
darkweb907 <barryopeyemi@gmail.com>
2022-06-04 23:54:47 +0100

    instagram clone

---
+++ database.js
@@ -0,0 +1,66 @@
+let request = indexedDB.open("instagram", 2);
+
+request.onupgradeneeded = () => {
+    console.log("upgrading....")
+    let db = request.result;
+    db.createObjectStore("post" , {autoIncrement: true})
+}
+
+
+const addPost = (postName, post) => {
+    let db = request.result;
+    let transaction = db.transaction([postName], 'readwrite')
+    let store = transaction.objectStore(postName);
+    store.add(post)
+    
+    transaction.oncomplete = () => {
+        console.log("transation Fetched successfully")
+    }
+}
+
+const clearPost = (postName) => {
+    let db = request.result;
+    let transaction = db.transaction([postName], 'readwrite')
+    let store = transaction.objectStore(postName);
+    store.clear()
+    
+    transaction.oncomplete = () => {
+        console.log("transation Fetched successfully")
+    }
+}
+
+const getPost = (postName) => {
+    let data = new Promise((resolve, reject) => {
+       let db = request.result;
+    let transaction = db.transaction([postName])
+    let getStore = transaction.objectStore(postName);
+        let postDetails = getStore.getAll()
+        console.log(postDetails)
+   
+    postDetails.onsuccess = () => {
+        console.log("gotten all post");
+        resolve(postDetails.result)
+    }  
+ })
+   return Promise.resolve(data)
+}
+
+let logo= document.querySelector(".brand")
+console.log(logo)
+
+request.onsuccess = () => {
+    let Btn= document.getElementById('form')
+    Btn.onsubmit = (e) => {
+        e.preventDefault()
+        let user = document.querySelector(".user").value
+        // window.location.href="index.html"
+        clearPost('post')
+        addPost("post", { user, food: "beans" })
+        
+    }
+    let head= document.getElementById('h1')
+    let myPost = getPost("post")
+    myPost.then(data => {
+        head.textContent= data[0].user
+    })
+}
